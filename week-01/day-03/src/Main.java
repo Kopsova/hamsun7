a4425e26697aaa9928ebd48947674bc06226cda3
Ivana <ivankanik@gmail.com>
2018-09-20 11:12:12 +0200

    Day 3 enviroment

---
+++ week-01/day-03/src/Main.java
@@ -0,0 +1,14 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int number = 5;
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Hello, user! Please give me your name");
+
+	// write your code here
+    }
+}
