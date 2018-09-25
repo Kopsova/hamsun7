8fcc9150375e006bc292db481ac2b443fbaded2a
Ivana <ivankanik@gmail.com>
2018-09-25 09:06:35 +0200

    Adding Matrix excercise

--- week-02/day-01/src/com/company/Excercise13.java
+++ week-02/day-01/src/com/company/Excercise13.java
@@ -2,7 +2,23 @@ package com.company;
 
 public class Excercise13 {
     public static void main (String[] args) {
-        int[][] matrix = new int[5][5];
-        for(int i)
+        int[][] matrix = new int[4][4];
+        for(int i=0; i<4; i++) {
+            for (int j=0; j<4; j++) {
+                if (i==j) {
+                    matrix[i][j] = 1;
+                } else {
+                    matrix[i][j] = 0;
+                }
+            }
+
+        }
+        for (int i = 0; i < 4; i++) {
+            String row = "";
+            for (int j = 0; j < 4; j++) {
+                row += matrix[i][j];
+            }
+            System.out.println(row);
+        }
     }
 }
---
+++ week-02/day-01/src/com/company/ExcerciseLoop.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class ExcerciseLoop {
+    public static void main(String[]args){
+        for (int i = 0; i<3; i++){
+            System.out.println("i=" +i);
+            for (int j=0; j<5; j++){
+                System.out.println("j=" +j);
+            }
+        }
+    }
+}
