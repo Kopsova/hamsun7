1735ac1907c092c25ae7f2fb647db0dca8c82fc4
Ivana <ivankanik@gmail.com>
2018-10-02 17:40:53 +0200

    Adding 'Copy file' excercise

--- week03/day02/src/com/company/Excercise06.java
+++ week03/day02/src/com/company/Excercise06.java
@@ -3,7 +3,28 @@ package com.company;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.util.List;
 
 public class Excercise06 {
-    
+    public static void main (String[] args) {
+        copyFiles("new-file.txt", "text.txt");
+
+    }
+
+
+    public static boolean copyFiles (String copyFrom, String copyTo) {
+        Path originalFile = Paths.get(copyFrom);
+        Path destinationFile = Paths.get (copyTo);
+
+        try {
+            List<String> lines = Files.readAllLines(originalFile);
+            Files.write(destinationFile, lines);
+            return true;
+        }catch(Exception e) {
+            return false;
+        }
+
+
+    }
+
 }
