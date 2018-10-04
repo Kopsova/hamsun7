25065e66a321524432614a3ba85370cc79c8076d
Ivana <ivankanik@gmail.com>
2018-10-04 18:31:58 +0200

    Adding more excercises, 1st String excercise

--- week03/day04/src/Excercise06.java
+++ week03/day04/src/Excercise06.java
@@ -1,7 +1,27 @@
 public class Excercise06 {
     public static void main (String[] args){
+        System.out.println(allEars(3));
 
     }
 
-    public static int 
-}
+    public static int allEars(int allbunnies) {
+        if (allbunnies == 0) {
+            return 0;
+        }else if (allbunnies%2 != 0){
+            return 2 + allEars(allbunnies - 1);
+        }
+
+        else {
+            return 3 + allEars(allbunnies - 1);
+        }
+
+
+
+
+
+                }
+            }
+
+
+
+
---
+++ week03/day04/src/Excercise07.java
@@ -0,0 +1,20 @@
+public class Excercise07 {
+    public static void main (String[] args) {
+        System.out.println(changingX("exykx"));
+
+    }
+
+    public static String changingX (String xxx) {
+        if (xxx.length() == 0) {
+            return xxx;
+        }else if (xxx.charAt(0) == 'x') {
+            return 'y'+ changingX(xxx.substring(1));
+
+        }
+        else {
+            return xxx.substring(0,1) + changingX(xxx.substring(1));
+        }
+
+
+    }
+}
