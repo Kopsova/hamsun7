0d1d888dc77b8930bc298dac4b74bd519d8bb06e
Ivana <ivankanik@gmail.com>
2018-10-22 19:43:33 +0200

    Adding more

--- week-01/day-03/src/com/company/Excercise28.java
+++ week-01/day-03/src/com/company/Excercise28.java
@@ -3,15 +3,26 @@ package com.company;
 import java.util.Scanner;
 
 public class Excercise28 {
-    public static void main(String [] args) {
+    // Write a program that reads a number from the standard input, then draws a
+// triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as the number was
+    public static void main(String[] args) {
+
         Scanner scenner = new Scanner(System.in);
-        System.out.println("Please, type number of rows");
+        System.out.println("Please type number of rows");
         int numbRows = scenner.nextInt();
-        for (int i=1; i <= numbRows; i++ ) {
-            for (int j= 1; j<= i; j++) {
-                System.out.print( "*");
+
+        for (int i = 1; i <= numbRows ; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print("*");
             }
-            System.out.println( );
+            System.out.println();
 
         }
 
